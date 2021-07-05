8790d71983fb4c7ca1ee826edd4e8430548f89cb
Piyush Maurya <55586404+iampiyushmaurya@users.noreply.github.com>
2021-07-05 19:07:33 +0530

    Add files via upload

---
+++ Day 16/src/components/Card.jsx
@@ -0,0 +1,11 @@
+const Card = () => {
+    return  (
+        <div className="card">
+            <img src="https://preview.redd.it/ppvzxt5bsd771.jpg?width=640&crop=smart&auto=webp&s=f41b5e781c7b96e35cc893a3d444726ade4bd095" alt="Prequel Meme" height="400px" width="400px"></img>
+            <h2>React Card</h2>
+            <p>This is meme card</p>
+        </div>
+    );
+};
+
+export default Card;

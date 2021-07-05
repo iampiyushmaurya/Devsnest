ab197aea210c4edf2cd0b9f71da324906cc81b2b
Piyush Maurya <55586404+iampiyushmaurya@users.noreply.github.com>
2021-07-05 19:14:49 +0530

    Add files via upload

---
+++ Day 16b/src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+    return  (
+        <div className="card">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/71h6wdQF2%2BL._AC_SL1500_.jpg"  alt="" height="400px" width="400px"></img>
+        </div>
+    );
+};
+
+export default Card;

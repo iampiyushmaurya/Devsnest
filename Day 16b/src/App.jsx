820b1c00b22de6885e820810fd1083b05cd9bc67
Piyush Maurya <55586404+iampiyushmaurya@users.noreply.github.com>
2021-07-12 17:16:59 +0530

    Add files via upload

---
+++ Day 16b/src/App.jsx
@@ -0,0 +1,87 @@
+import Box1 from "./Box1";
+import Box2 from "./Box2";
+import "./style.css";
+
+
+const App = () =>{
+
+    return (
+        <div className="App">
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+            <Box2 />
+            <Box1 />
+
+        </div>
+    )
+
+}
+
+export default App;
---
+++ Day 16b/src/Box2.jsx
@@ -0,0 +1,10 @@
+import "./style.css";
+   
+const Box2 = () =>{
+    return (
+        <div className="Box2">
+        </div>
+    )
+}
+
+export default Box2;

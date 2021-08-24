dfb278d410fa3db0be67a3c384084ae19ba873da
Piyush Maurya <55586404+iampiyushmaurya@users.noreply.github.com>
2021-08-25 03:49:39 +0530

    Add files via upload

---
+++ Day 24/src/router/Dashboard.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react'
+import {
+    Route,
+    Redirect,
+} from "react-router-dom"
+import Context from '../context/AuthContext'
+
+
+
+function Dashboard(props) {
+
+    const logger = useContext(Context)
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <Route>
+                {
+                    logger.loggedIn?"Welcome to Dashboard" : <Redirect to="/" />
+                }
+
+            </Route>
+        </div>
+    )
+}
+
+export default Dashboard
